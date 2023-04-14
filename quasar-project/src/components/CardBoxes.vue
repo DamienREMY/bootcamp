13d30b877d1bbbb0fdb28a5ce967a2670ff92b5c
Rdamien <damien.remy.vezon@gmail.com>
2023-04-14 15:52:59 +0200

    final updates

--- quasar-project/src/components/CardBoxes.vue
+++ quasar-project/src/components/CardBoxes.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card v-for="language in dataJson" :key="language.id" class="my-card">
+      <img
+        :src="language.text"
+        :alt="language.label"
+        style="width: 100px; height: 100px"
+      />
+      <q-card-section>
+        <div class="text-h6">{{ language.label }}</div>
+      </q-card-section>
 
-      <q-card v-for="language in dataJson" :key="language.id" class="my-card">
-        <img :src="require('../assets/JS_logo.png')" alt=language.text style="width: 100px; height: 100px" />        
-        <q-card-section>
-          <div class="text-h6">{{language.label}}</div>
-        </q-card-section>
-  
-        <q-card-section class="q-pt-none">
+      <q-card-section class="q-pt-none"> </q-card-section>
+    </q-card>
+  </div>
+</template>
 
-        </q-card-section>
-      </q-card>
-  
-      
-    </div>
-  </template>
-  
-  <script>
-import { defineComponent } from 'vue';
-import json from "../assets/data.json"
+<script>
+import { defineComponent } from "vue";
+import json from "../assets/data.json";
 
-  export default defineComponent({
-    name:'CardBoxes.vue',
-    setup () {
-      return {
-        lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      }
-    },
-    data () {
-        return {dataJson:json.techno}
-    }
+export default defineComponent({
+  name: "CardBoxes.vue",
+  setup() {
+    return {
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    };
+  },
+  data() {
+    return {
+      dataJson: json.techno,
+    };
+  },
+});
+</script>
 
-  })
-  </script>
-  
-  <style lang="sass" scoped>
-  .my-card
-    width: 100%
-    max-width: 250px
-  </style>
-  
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
---
+++ tourofheroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+
+    { id: 2, name: 'Booba (le petit ourson)' },
+    { id: 3, name: 'Evil Piwi' },
+    { id: 4, name: 'Dio' },
+    { id: 5, name: 'Segpaman' },
+    { id: 6, name: 'Omnignôle' },
+    { id: 7, name: 'Zêtabête' },
+
+]
